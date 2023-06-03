ebc12b1a390ab36d8dcd020b45410da282fa8d60
Miguel Grinberg <miguel.grinberg@gmail.com>
2023-06-03 19:38:59 +0100

    Duplicate the gevent socket to avoid using it in multiple greenlets (Fixes #24)

--- src/simple_websocket/ws.py
+++ src/simple_websocket/ws.py
@@ -313,7 +313,7 @@ class Server(Base):
             if not hasattr(wsgi_input, 'raw') and hasattr(wsgi_input, 'rfile'):
                 wsgi_input = wsgi_input.rfile
             if hasattr(wsgi_input, 'raw'):
-                sock = wsgi_input.raw._sock
+                sock = wsgi_input.raw._sock.dup()
                 self.mode = 'gevent'
         if sock is None:
             raise RuntimeError('Cannot obtain socket from WSGI environment.')
